c66904a150a9f01dc19c82e1f56628b1f6999809
nikolaevatatyana <t.nikolaeva1@g.nsu.ru>
2022-03-14 19:05:17 +0700

    14032022commit3

--- app/src/main/java/com/nikolaeva/healthdiary/ChallengesFragment.kt
+++ app/src/main/java/com/nikolaeva/healthdiary/ChallengesFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 
 class ChallengesFragment : Fragment() {
@@ -16,6 +18,30 @@ class ChallengesFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_challendes, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val arrayAdapter: ArrayAdapter<*>
+
+        val users = arrayOf(
+
+            "Challenge1", "Challenge2", "Challenge3",
+
+            "Challenge4", "Challenge5"
+
+        )
+
+        // доступ к listView из файла XML
+
+        var mListView = view.findViewById<ListView>(R.id.userList)
+
+        /*arrayAdapter = ArrayAdapter(this,
+
+            android.R.layout.simple_list_item_1, users)
+
+        mListView.adapter = arrayAdapter*/
     }
 
     companion object {
