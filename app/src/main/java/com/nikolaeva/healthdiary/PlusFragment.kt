b56225c01ecea36320499a139579932037c5b530
nikolaevatatyana <t.nikolaeva1@g.nsu.ru>
2022-03-23 12:50:13 +0700

    23032022commit

---
+++ app/src/main/java/com/nikolaeva/healthdiary/PlusFragment.kt
@@ -0,0 +1,27 @@
+package com.nikolaeva.healthdiary
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class PlusFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_plus, container, false)
+    }
+
+    companion object {
+
+        fun newInstance() = PlusFragment()
+
+    }
+}
